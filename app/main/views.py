19c403b505d2acf9a1e1edd123c8539da8e22ebc
Anas-Tawalbeh <59394028+Anas-Tawalbeh@users.noreply.github.com>
2025-09-27 14:06:27 +0700

    “update

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,8 @@ from flask import render_template
 from flask_login import login_required, current_user
 from . import main
 
+
+
 @main.route('/')
 def index():
     return render_template("main/index.html")
